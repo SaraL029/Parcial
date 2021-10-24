be03f6f9300c2de8db213d4a63d28fe2b9b4f1cf
SaraL029 <93046247+SaraL029@users.noreply.github.com>
2021-10-24 00:50:12 -0500

    Créditos.cs
    Se agregaron los créditos, si hay algún error con los ids y los nombres perdón y corrijanlos :(

--- Menu/clases/clsOpcion8.cs
+++ Menu/clases/clsOpcion8.cs
@@ -11,7 +11,14 @@ namespace Menu.clases
             Console.Clear();
             Console.WriteLine("Has escogido la opción de Acerca de");
             Console.ReadKey();
-            // fundamentos de programación bla bla bla
+            
+            Console.WriteLine("\n" + "Proyecto creado por:" + "\n" +
+             "\n" + "AnaMaría Díaz Rocha " + " ID: 000436459" +
+             "\n" + "Sebastián Bello Garcia" + " ID: 000385812" +
+             "\n" + "Sara Londoño Orrego " + " ID: 000437687" + "\n" +
+             "\n" + "Para la clase de Fundamentos de Programación con el profesor Andrés Bedoya Tobón los martes y jueves a las 10am"+ "\n"+
+             "\n" + "Universidad Pontificia Bolivariana" + "\n"+ "2021");
+   
         }
     }
 }
